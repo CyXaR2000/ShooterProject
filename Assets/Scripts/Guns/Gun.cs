6627df3d1a65b68fa3b5917b8e6eddbf046b9259
Vadim <vadim.vorontsov@mail.ru>
2024-06-29 20:16:18 +0300

    Level_2

--- Assets/Scripts/Guns/Gun.cs
+++ Assets/Scripts/Guns/Gun.cs
@@ -1,17 +1,21 @@
 using UnityEngine;
 
-[RequireComponent (typeof(RayShooter))]
+[RequireComponent(typeof(RayShooter))]
 public class Gun : MonoBehaviour
 {
     [SerializeField] InputReader _inputReader;
     [SerializeField] SpriteAnimator _spriteAnimator;
+    [SerializeField] private AudioSource _fireShot;
+    [SerializeField] private AudioSource _recharge;
 
     private RayShooter _rayShooter;
 
     private void Awake()
     {
         _rayShooter = GetComponent<RayShooter>();
+
     }
+
 
     private void OnEnable()
     {
@@ -25,5 +29,15 @@ public class Gun : MonoBehaviour
         _inputReader.ZeroMouseButtomPressed -= _rayShooter.Fire;
         _inputReader.ZeroMouseButtomPressed -= _spriteAnimator.Shot;
         _inputReader.MoveButtonPressed -= _spriteAnimator.Walk;
+    }
+
+    public void FireSource()
+    {
+        _fireShot.Play();
+    }
+
+    public void RechargeSource()
+    {
+        _recharge.Play();
     }
 }
